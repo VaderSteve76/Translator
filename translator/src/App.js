2fe42215a03971121557f5b091fa5119529aee2d
VaderSteve76 <alverson.s@yahoo.com>
2021-08-12 10:56:35 -0400

    Input for translation

--- translator/src/App.js
+++ translator/src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Field from './components/field';
 import './App.css';
 
 export default function App() {
+  const [language, setLanguage] = useState("ru");
+  const [text, setText] = useState("");
   return (
     <div>
-      
+      <Field label="Enter English" onChange={setText} value={text} />
     </div>
   )
 }
