d88c5f0fa8afe6fe6ba3d358235c09177953fed4
VaderSteve76 <alverson.s@yahoo.com>
2021-08-12 11:14:57 -0400

    Added languages

--- translator/src/App.js
+++ translator/src/App.js
@@ -3,7 +3,7 @@ import Field from './components/field';
 import './App.css';
 
 export default function App() {
-  const [language, setLanguage] = useState("ru");
+  const [language, setLanguage] = useState("es");
   const [text, setText] = useState("");
   return (
     <div>
